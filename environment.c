de664ecac58924b11183a8770af33742edc87400
gharnie <62819684+gharnie@users.noreply.github.com>
2022-05-21 00:48:09 +0100

    Create environment.c

---
+++ environment.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+/**
+ * make_env - Make the shell environment from the environment passed to main
+ * @env: Environment passed to main
+ *
+ * Return: Pointer to the new environment
+ */
+char **make_env(char **env)
+{
+	char **newenv = NULL;
+	size_t i;
+
+	for (i = 0; env[i] != NULL; i++)
+		;
+	newenv = malloc(sizeof(char *) * (i + 1));
+	if (newenv == NULL)
+	{
+		perror("Fatal Error");
+		exit(1);
+	}
+	for (i = 0; env[i] != NULL; i++)
+		newenv[i] = _strdup(env[i]);
+	newenv[i] = NULL;
+	return (newenv);
+}
+
+/**
+ * free_env - Free the shell's environment
+ * @env: Shell's environment
+ *
+ * Return: void
+ */
+void free_env(char **env)
+{
+	unsigned int i;
+
+	for (i = 0; env[i] != NULL; i++)
+		free(env[i]);
+	free(env);
+}
